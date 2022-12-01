f3bd4fb3e5fafe15419808464f1ee8bd531e2a10
Slacked Lime <slackedlime@proton.me>
2022-12-02 00:24:46 +0400

    Added tab to focus

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 import * as wiki from "./wikipedia.js";
 
+const websiteName = document.location.origin;
+
 const search = document.getElementById("search");
 const autoComplete = document.getElementById("auto-complete");
 
@@ -7,41 +9,6 @@ const summaryTitle = document.getElementById("title");
 const summaryParagraph = document.getElementById("summary");
 const thumbnail = document.getElementById("thumbnail");
 const thumbnailExplaination = document.getElementById("thumbnail-explaination");
-
-search.addEventListener("input", () => {
-	if (!search.value) {
-		autoComplete.style.display = "none";
-		return;
-	} else {
-		autoComplete.style.display = "block";
-	}
-
-	wiki.getSearchItems(search.value).then(items => {
-		autoComplete.textContent = "";
-		
-		if (!items.length) {
-			autoComplete.appendChild(document.createTextNode("No Results Found"));
-			return;
-		}
-
-		for (let item of items) {
-			let list = document.createElement("li");
-			list.className = "search-items";
-			list.addEventListener("click", loadPage)
-			list.appendChild(document.createTextNode(item));
-			autoComplete.appendChild(list);
-		}
-	});
-});
-
-document.onkeydown = function(event) {
-	event = event || window.event;
-	if (event.code == "Escape") {
-		autoComplete.style.display = "none";
-	} else if (event.code == "Enter" && document.activeElement === search) {
-		loadPage({target: autoComplete.firstChild});
-	}
-};
 
 function loadPage(event) {
 	let pageName = event.target.textContent;
@@ -62,17 +29,81 @@ function loadPage(event) {
 	});
 }
 
+async function closeSearch() {
+	await new Promise(r => setTimeout(r, 200));
+
+	if (!autoComplete.contains(document.activeElement)) {
+		autoComplete.style.display = "none";
+	}
+}
+
+search.addEventListener("input", () => {
+	if (!search.value) {
+		autoComplete.style.display = "none";
+		return;
+	} else {
+		autoComplete.style.display = "block";
+	}
+
+	wiki.getSearchItems(search.value).then(items => {
+		autoComplete.textContent = "";
+		
+		if (!items.length) {
+			autoComplete.appendChild(document.createTextNode("No Results Found"));
+			return;
+		}
+
+		for (let item of items) {
+			let list = document.createElement("li");
+
+			list.className = "search-items";
+			list.role = "button";
+			list.tabIndex = "0"
+
+			list.addEventListener("click", loadPage);
+			list.addEventListener("focusout", closeSearch);
+
+			list.appendChild(document.createTextNode(item));
+			autoComplete.appendChild(list);
+		}
+	});
+});
+
 search.addEventListener("focusin", () => {
 	autoComplete.style.display = "block";
 });
 
-search.addEventListener("focusout", async () => {
-	await new Promise(r => setTimeout(r, 200));
-	autoComplete.style.display = "none";
-});
+search.addEventListener("focusout", closeSearch);
+
+document.onkeydown = (event) => {
+	event = event || window.event;
+	
+	if (event.code == "Escape") {
+		autoComplete.style.display = "none";
+	}
+
+	if (event.code != "Enter") {
+		return;
+	}
+
+	if (document.activeElement == search) {
+		loadPage({target: autoComplete.firstChild});
+
+	} else if (autoComplete.contains(document.activeElement)) {
+		loadPage({target: document.activeElement});
+	}
+
+};
+
+document.onkeyup = (event) => {
+	event = event || window.event;
+
+	if (event.code == "Slash") {
+		search.focus();
+	}
+}
 
 let params = new URLSearchParams(window.location.search);
-
 let page = params.has("page") ? params.get("page") : "Mimir";
 
 loadPage({target: {textContent: page}});
